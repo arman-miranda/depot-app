2045fcc33c553d7139e6dd793860164dd96552f3
armanmiranda <arman.miranda.ust@gmail.com>
2017-06-07 14:42:03 +0800

    add time and date in the side bar

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,6 +15,10 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '.list_description dl dt', 'Programming Ruby 1.9'
+    assert_select '.list_line_odd', 2
+    assert_select '.list_line_even', 1 
+    assert_select '.list_image', 3
   end
 
   test "should get new" do
