d84827d07a50c97b23ab959ad204571459ab16ff
armanmiranda <arman.miranda.ust@gmail.com>
2017-06-09 13:54:27 +0800

    add playtime features

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,5 +1,27 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  
+  test "add unique products" do
+  	cart = Cart.create
+  	book_one = products(:one)
+  	book_two = products(:two)
+
+  	cart.add_product(book_one.id).save!
+  	cart.add_product(book_two.id).save!
+
+  	assert_equal 2, cart.line_items.size
+  	assert_equal book_one.price + book_two.price, cart.total_price
+  end
+
+  test "add duplicate products" do
+  	cart = Cart.create
+  	book_one = products(:one)
+  	book_dup_one = products(:one)
+
+  	cart.add_product(book_one.id).save!
+  	cart.add_product(book_dup_one).save!
+
+  	assert_equal 1, cart.line_items.size
+  	assert_equal book_one.price + book_dup_one.price, cart.total_price
+  end
 end
